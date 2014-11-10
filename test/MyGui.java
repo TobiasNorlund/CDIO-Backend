f3cdbd2e251b5f7ad3e110d7849bb78e16d8e752
KingeranXL <lukastallund@gmail.com>
2014-11-10 17:47:27 +0100

    Fixed output error and added possibility to lower bound on log dates

--- test/MyGui.java
+++ test/MyGui.java
@@ -1,30 +1,16 @@
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
-import java.util.Iterator;
-import java.util.ListIterator;
-
 import edu.wildlifesecurity.backend.repository.impl.FileRepository;
 import edu.wildlifesecurity.backend.ISystemInterface; 
 import edu.wildlifesecurity.framework.repository.IRepository;
 import javafx.application.Application;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBase;
 import javafx.scene.control.DatePicker;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
@@ -65,20 +51,21 @@ public class MyGui extends Application implements ISystemInterface {
          DatePicker startDatePicker=(DatePicker) scene.lookup("#start_date");
          DatePicker endDatePicker=(DatePicker) scene.lookup("#end_date");
          
-         
-         stage.show();
-         
-         
-         
-         log.setText("This is a text sample");
          textFlow.getChildren().addAll(log);
+         stage.show();
 
          
          startDatePicker.setOnAction(event -> {
      	   LocalDate date = startDatePicker.getValue();
-     	  startDate = Date.from(date.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
-           log.setText(fr.getLog(startDate));
-           textFlow.getChildren().addAll(log);
+     	   startDate = Date.from(date.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+           log.setText(fr.getLog(startDate,endDate));
+
+     	});
+         
+         endDatePicker.setOnAction(event -> {
+     	   LocalDate date = endDatePicker.getValue();
+     	   endDate = Date.from(date.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+           log.setText(fr.getLog(startDate,endDate));
 
      	});
 
