9c1e71dcacd9726c782c7769989fdfa6a3b88e5f
TobiasNorlund <tobias.norlund@tele2.se>
2014-11-26 16:39:58 +0100

    Lade tillbaka så att loggen uppdateras

--- src/edu/wildlifesecurity/backend/sysinterface/gui/view/LogViewController.java
+++ src/edu/wildlifesecurity/backend/sysinterface/gui/view/LogViewController.java
@@ -39,28 +39,28 @@ public class LogViewController {
     private void initialize() {
     	logEnd=new Date();
     	logStart=new Date(0);
-//		MainApp.repository.addEventHandler(LogEvent.INFO, new IEventHandler<LogEvent>(){
-//		
-//		        	
-//					@Override
-//					public void handle(LogEvent event) {
-//						Platform.runLater(new Runnable() {
-//						    @Override
-//						    public void run() {
-//						    	if (!forced)
-//						    	{
-//							    	System.out.println("List of log entries updated!");
-//							    	logEnd=new Date();
-//							    	reloadLog();
-//						    	}
-//						    }
-//						});
-//				
-//				
-//				
-//			}
-//        	
-//        });
+		MainApp.repository.addEventHandler(LogEvent.INFO, new IEventHandler<LogEvent>(){
+		
+		        	
+					@Override
+					public void handle(LogEvent event) {
+						Platform.runLater(new Runnable() {
+						    @Override
+						    public void run() {
+						    	if (!forced)
+						    	{
+							    	System.out.println("List of log entries updated!");
+							    	logEnd=new Date();
+							    	reloadLog();
+						    	}
+						    }
+						});
+				
+				
+				
+			}
+        	
+        });
     }
     
     @FXML
